05342fafc18bf0ecbc4e1e0787e77663e2c32c40
youssef-hossam <yousseftube126@gmail.com>
2024-03-28 01:46:32 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:noteapp/views/notes_view.dart';
+
+void main() {
+  runApp(NoteApp());
+}
+
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: const NotesView(),
+    );
+  }
+}
