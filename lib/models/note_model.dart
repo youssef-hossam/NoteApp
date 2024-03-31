02c91a55f71b6234b176924d204a31d478c58d5f
youssef-hossam <yousseftube126@gmail.com>
2024-03-31 23:45:30 +0200

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,12 @@
+class NoteModel {
+  final String title;
+  final String subtitle;
+  final String date;
+  final int color;
+  NoteModel({
+    required this.color,
+    required this.date,
+    required this.subtitle,
+    required this.title,
+  });
+}
